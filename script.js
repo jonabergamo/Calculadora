84069a13dda5914b245f88e14c5b39139975022d
jonabergamo <jonathanbergamo16@gmail.com>
2023-01-28 04:07:42 -0300

    file js added

--- script.js
+++ script.js
@@ -5,3 +5,41 @@ function tecla (num)
 
 }
 
+
+    function tecla (num)
+    {
+        var numero = document.getElementById('resultado').innerHTML
+        document.getElementById('resultado').innerHTML = numero + num
+
+     }
+
+     function clean() 
+     {
+        document.getElementById('resultado').innerHTML = ''
+        document.getElementById('digitado').innerHTML = ''
+     }
+
+
+     function back()
+     {
+        var digitado = document.getElementById('digitado').innerHTML
+        var resultado = document.getElementById('resultado').innerHTML;
+        document.getElementById('resultado').innerHTML = resultado.substring(0, resultado.length -1)
+        
+        if (resultado.length == 1) {
+            clean()
+        }
+     }
+
+     function calcular()
+     {
+        var resultado = document.getElementById('resultado').innerHTML
+        var digitado = document.getElementById('digitado').innerHTML
+        if(resultado)
+        {
+            document.getElementById('digitado').innerHTML = resultado
+            document.getElementById('resultado').innerHTML = eval(resultado)
+
+        }
+     }
+
